9b9bde7393591973893dffc41b31cfa8d8b27914
SirGreat <dyiesmathew27@gmail.com>
2023-09-08 23:38:08 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
 /**
- * main-this is the main put function
- * Return: (0)
+ * main-main entry
+ * Return: 0
  */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");return (0); 
+	puts("\"programming is like building a multilingual puzzle");
+	return (0);
 }
---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main-this describes the main function
+ * Return: 0
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a peace of art,\n");
+	return (0);
+}
