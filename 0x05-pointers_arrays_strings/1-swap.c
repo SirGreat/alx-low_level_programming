12d08540b7e9ddb010be8477bfa8269ec65e2291
SirGreat <dyiesmathew27@gmail.com>
2023-09-18 15:05:21 -0700

    1-swap.c

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+  * swap_int-function name
+  * @n: first parameter
+  * @n: second parameter
+  */
+
+void swap_int(int *a, int *b)
+{
+int c;
+c = *a;
+*a = *b;
+*b = c;
+
+
+
+}
+
+
+
