a72b5d333ecaa7bbf31888f1315163e160cba255
SirGreat <dyiesmathew27@gmail.com>
2023-09-18 14:30:32 -0700

    0-reset_to_98.c

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * reset_to_98-this is the function name
+ *
+ * @n: the value of a deferencing pointer
+ *
+ * Return: nth
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
