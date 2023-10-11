17fd5ebc54e9d3901675c96a6ba37ddd19212fdf
SirGreat <dyiesmathew27@gmail.com>
2023-10-10 19:55:08 -0700

    File name

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,6 +1,13 @@
 #include <stdio.h>
 
-int main(int argc, char *argv[]) {
-    printf("%s\n", argv[0]);
-    return 0;
+/**
+ * main - prints the name of the file
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
 }
+
